f758f309ae7c6e09153f1e02f6e7f5de054b7dd2
Mirko <mirko.maelicke@kit.edu>
2020-12-07 17:09:32 +0100

    logger.py update

--- raspi/scripts/logger.py
+++ raspi/scripts/logger.py
@@ -6,39 +6,55 @@ from time import time, sleep
 
 from ds18b20 import read_sensor
 from util import parse_interval_to_seconds, config
+from sqlite_backend import append_data
 
 
-def save_data(path=None, in_soil=False, dry=False, **kwargs):
+def _save_json_backend(path, new_data, conf):
     # create a new file per day
     if path is None:
         date = dt.now().date()
         fname = '%d_%d_%d_raw_log.json' % (date.year, date.month, date.day)
 
         # get path
-        conf = config()
         path = pjoin(conf.get('loggerPath', pjoin(os.path.expanduser('~'), 'logger')), fname)
 
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
 
-    # build up an in_soil list if needed
-    data = []
-    if isinstance(in_soil, (list, tuple)):
-        for s in in_soil:
-            data.extend(read_sensor(in_soil=s, **kwargs))
-    else:
-        data = read_sensor(in_soil=in_soil, **kwargs)
-    
     # save data
-    if not dry:
-        try:
-            with open(path, 'r') as f:
-                old_data = json.load(f)
-        except:
-            old_data = []
-        with open(path, 'w') as js:
-            old_data.extend(data)
-            json.dump(old_data, js, indent=4)
+    # get existing data
+    try:
+        with open(path, 'r') as f:
+            old_data = json.load(f)
+    except:
+        old_data = []
+    
+    # append and save
+    with open(path, 'w') as js:
+        old_data.extend(new_data)
+        json.dump(old_data, js, indent=4)
+
+
+def save_data(path=None, dry=False, **kwargs):
+    # get the config
+    conf = config()
+
+    # read the sensor data
+    # TODO, here the protocols could be loaded from config.
+    data = read_sensor(conf=conf, **kwargs)
+    
+    # in dry runs, only return the data
+    if dry:
+        return data
+    
+    # check the registered backends
+    for name, c in conf.get('loggerBackends', {}).items():
+        # check if the backend is currently enabled
+        if c.get('enabled', True):
+            if name == 'json':
+                _save_json_backend(path, data, conf)
+            elif name == 'sqlite3':
+                append_data(data, conf, path)
 
     # return the data for reuse
     return data
@@ -65,36 +81,22 @@ def stream(interval=None, dry=False, **kwargs):
     print(outstr)
 
     # sleep for the remaining time
-    sleep(interval - (time() - t1))
+    remain = interval - (time() - t1)
+    if remain < 0:
+        remain = 0
+    sleep(remain)
 
     # call again
     stream(dry=dry, **kwargs)
 
 
-def run(interval=None, **kwargs):
-    # get the start time
-    t1 = time()
-    
-    if interval is None:
-        interval = config().get('loggerInterval', '1min')
-    else:
-        config(loggerInterval=interval)
-    
-    if isinstance(interval, str):
-        interval = parse_interval_to_seconds(interval)
-    
-    save_data(dry=False, **kwargs)
-
-    # sleep for the remaining time
-    sleep(interval - (time() - t1))
-
-    # call again
-    run(**kwargs)
-
+def run(action: 'log' | 'activate' | 'deactive'='log', **kwargs):
+    raise NotImplementedError
 
 if __name__=='__main__':
     import fire
     fire.Fire({
         'save': save_data,
-        'stream': stream
+        'stream': stream,
+        'run': run
     })
